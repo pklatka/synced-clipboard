c0548b8cd52e7b41dffda436a5063dc41d48d87e
Patryk Klatka <patryk.klatka@gmail.com>
2023-02-13 18:36:14 +0100

    Refactor renderer.ts

--- desktop/src/public/renderer.ts
+++ desktop/src/public/renderer.ts
@@ -1,12 +1,14 @@
 document.addEventListener('DOMContentLoaded', async () => {
+    const electronAPI = (window as any).electronAPI // Temporary solution with any type.
+
     // Show server address.
-    document.getElementById('server-address').innerHTML = await (window as any).electronAPI.getLocalIPAddress()
+    document.getElementById('server-address').innerHTML = await electronAPI.getLocalIPAddress()
 
     // Show client list every 3 seconds.
     const clientListElement = document.getElementById('client-list')
 
     setInterval(async () => {
-        const clientList: Array<string> = await (window as any).electronAPI.getClientList()
+        const clientList: Array<string> = await electronAPI.getClientList()
         if (clientList.length <= 0) {
             clientListElement.innerHTML = `<div class="client">No clients connected</div>`
             return;
